21d5515f5bc9057dbec60c8d1ebc655b6b33f277
Nathan Mull <nmmull@gmail.com>
2026-01-19 17:54:55 -0500

    updates for 1

--- build.py
+++ build.py
@@ -1,60 +1,20 @@
+import stdlib132
 from stdlib132 import probgen
 
 with open('main-template.tex', 'r') as f:
     body = f.read()
 
+rref = stdlib132.random.rref(2, 3, seed=1722463322)
+rref2 = stdlib132.random.rref(3, 5, seed=236229483)
+
 problems = [
     (
-        "determineCoefficientAugmentedMatrix2",
-        probgen.determine_coefficient_augmented_matrix(
-            num_eqs=2,
-            num_vars=2,
-            seed=2009837464,
-        )
+        "input",
+        stdlib132.latex.matrix(rref)
     ),
     (
-        "determineCoefficientAugmentedMatrix1",
-        probgen.determine_coefficient_augmented_matrix(
-            num_eqs=3,
-            num_vars=3,
-            seed=3209557267,
-        )
-    ),
-    (
-        "determineCoefficientAugmentedMatrix3",
-        probgen.determine_coefficient_augmented_matrix(
-            num_eqs=4,
-            num_vars=5,
-            seed=2878334791,
-        )
-    ),
-    (
-        "determineUniqueSolutionLinearSystem1",
-        probgen.determine_unique_solution_linear_system(
-            num_eqs=2,
-            seed=3094789175,
-        )
-    ),
-    (
-        "determineUniqueSolutionLinearSystem2",
-        probgen.determine_unique_solution_linear_system(
-            num_eqs=3,
-            seed=425464091,
-        )
-    ),
-    (
-        "determineUniqueSolutionLinearSystem3",
-        probgen.determine_unique_solution_linear_system(
-            num_eqs=3,
-            seed=2573908387,
-        )
-    ),
-    (
-        "determineUniqueSolutionLinearSystem4",
-        probgen.determine_unique_solution_linear_system(
-            num_eqs=4,
-            seed=4148622710,
-        )
+        "input2",
+        stdlib132.latex.matrix(rref2)
     ),
 ]
 
