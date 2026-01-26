9db19a0739f135c4224a68bc6639d363d3965912
Nathan Mull <nmmull@gmail.com>
2026-01-25 22:23:55 -0500

    added gen form problems

--- build.py
+++ build.py
@@ -5,11 +5,10 @@ with open('main-template.tex', 'r') as f:
     body = f.read()
 
 problems = [
-    probgen.equiv_vector_eq(3, 3, 2356493351),
-    probgen.equiv_vector_eq(2, 4, 1191322468),
+    probgen.gen_form_sol_vec_eq(3, 4, 2295471575),
+    probgen.gen_form_sol_vec_eq(2, 4, 3131868451),
+
 ]
-# 1444954305
-# 465354056
 
 for i, problem in enumerate(problems):
     body = body.replace(f"<<{i}>>", problem)
