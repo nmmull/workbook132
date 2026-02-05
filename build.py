beaefffe50f41521a744fc1a61e1ef49ab09f20c
Nathan Mull <nmmull@gmail.com>
2026-02-05 08:12:49 -0500

    added new problem

--- build.py
+++ build.py
@@ -4,11 +4,17 @@ from stdlib132 import probgen
 with open('main-template.tex', 'r') as f:
     body = f.read()
 
-problems = [
+subs = [
+  stdlib132.latex.matrix(
+    stdlib132.random.int_matrix(3, 3)
+  ),
+  stdlib132.latex.matrix(
+    stdlib132.random.int_matrix(3, 1)
+  )
 ]
 
-for i, problem in enumerate(problems):
-    body = body.replace(f"<<{i}>>", problem)
+for i, sub in enumerate(subs):
+    body = body.replace(f"<<{i}>>", sub)
 
 with open('main.tex', 'w') as f:
     f.write(body)
